f3a18dca7faac81614deb26ee59c16af5c1c00a5
Stuart Axon <stuaxo2@yahoo.com>
2012-11-18 16:59:58 +0000

    Add more info to README and setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from distutils.core import setup
 setup(name='add-dist-packages',
       version='0.1',
       scripts=['add-dist-package'],
+      license='LICENSE',
+      description='Create symlinks to system packages inside the current or specified virtualenv'
+      long_description=open('README.txt').read(),
       )
