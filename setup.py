ca57d077e47a17f8cdf6d96b2835bd88a83f9cf1
stu <stuart.axon@jpcreative.co.uk>
2012-11-19 09:51:20 +0000

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='add-dist-packages',
       scripts=['add-dist-package'],
       license='LICENSE',
       description='Create symlinks to system packages inside the current or specified virtualenv'
-      long_description=open('README.txt').read(),
+      long_description=open('README').read(),
       author='Stuart Axon',
       author_email='stu.axon+adp@gmail.com',
       )
